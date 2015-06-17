0af4849976c6df4a036b91e8b9cb17264e73833f
Jack Frost <kodeandplay@gmail.com>
2015-06-16 19:46:30 -0700

    Moved update and draw method to prototype of Particle in order to improve performance and take advantage of prototypical inheritance provided by Javascript

--- explosion.js
+++ explosion.js
@@ -3,7 +3,7 @@ var canvas, ctx, particles = [];
 
 window.addEventListener('load',function() {
   canvas = document.getElementById('canvas');
-  ctx = canvas.getContext('2d'); 
+  ctx = canvas.getContext('2d');
 });
 
 function Particle(center) {
@@ -16,30 +16,31 @@ function Particle(center) {
   this.velocityY = 0;
   this.scaleSpeed = 0.5;
   this.center = center;
-  
-  this.update = function(ms) {
-    //shrinking
-    this.scale -= this.scaleSpeed * ms / 1000;
-    
-    if(this.scale <= 0) { this.scale = 0; }
-    
-    //moving away from explosion
-    this.x += this.velocityX * ms / 1000;
-    this.y += this.velocityY * ms / 1000;
-  }
-  
-  this.draw = function(ctx) {
-    // translating the 2D context to the particle coordinates
-    ctx.save();
-    ctx.translate(this.x*10,this.y*10);
-    ctx.scale(this.scale,this.scale);
-    
-    // draw a filled square in the particle's local space
-    ctx.fillStyle = this.color;
-    ctx.beginPath();
-    ctx.fillRect(0,0,this.side,this.side);
-    ctx.restore();
-  }
+
+}
+
+Particle.prototype.update = function(ms) {
+  //shrinking
+  this.scale -= this.scaleSpeed * ms / 1000;
+
+  if(this.scale <= 0) { this.scale = 0; }
+
+  //moving away from explosion
+  this.x += this.velocityX * ms / 1000;
+  this.y += this.velocityY * ms / 1000;
+}
+
+Particle.prototype.draw = function(ctx) {
+  // translating the 2D context to the particle coordinates
+  ctx.save();
+  ctx.translate(this.x*10,this.y*10);
+  ctx.scale(this.scale,this.scale);
+
+  // draw a filled square in the particle's local space
+  ctx.fillStyle = this.color;
+  ctx.beginPath();
+  ctx.fillRect(0,0,this.side,this.side);
+  ctx.restore();
 }
 
 function randomFloat(min,max) {
@@ -50,7 +51,7 @@ function createExplosion(loc,color,callback) {
   var minSize = 4, maxSize = 18;
   var minSpeed = 10, maxSpeed = 40;
   var minScaleSpeed = 1.0, maxScaleSpeed = 4.0;
-  
+
   for(var angle = 0; angle < 360; angle += 36) {
     var particle = new Particle(loc);
     particle.side = randomFloat(minSize,maxSize);
@@ -59,10 +60,10 @@ function createExplosion(loc,color,callback) {
     var speed = randomFloat(minSpeed,maxSpeed);
     particle.velocityX = speed * Math.cos(angle*Math.PI/180);
     particle.velocityY = speed * Math.sin(angle*Math.PI/180);
-    
+
     particles.push(particle);
   }
-  
+
   setTimeout(callback,1000);
 }
 
@@ -73,28 +74,3 @@ function update(frameDelay) {
     particle.draw(ctx);
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
